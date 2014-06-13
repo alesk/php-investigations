13160297230f35609dc8d94bcd48e9fa2a5b2371
Aleš Kotnik <ales.kotnik@gmail.com>
2014-06-13 08:47:58 +0000

    Test asumptions about passing by value or by reference.

---
+++ PassingByValueOrByReference.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * Assumption:
+ *
+ * Arrays if passed by value are copied and changing the value does not 
+ * affect the value at the original array.
+ * See `testArrayPassingByValue`.
+ *
+ * If array is passed by reference, mutating passed array mutates original.
+ * See `testArrayPassingByReference`.
+ *
+ * If array contains objects, the copy of aray will contain copies of objects
+ * identifiers which are still pointing to the original object.
+ * See `testArrayPAssingByValueCotainingObjects`.
+ *
+ * Variables pointing to object are actually pointing to object identifiers,
+ * so after a copy, the new copy still contains the same object identifier
+ * and effectively "points" to the same object. That's why passing by value
+ * or passing by reference makes no difference.
+ **/
+
+class A { public $foo = 1;}
+
+class PassingByValueOrByReferenceTest extends PHPUnit_Framework_TestCase {
+
+  public function testArrayPassingByValue() {
+
+    $a = [1, 2];
+
+    $passByValue = function($array) {$array[0] = 100;};
+
+    $this->assertEquals(1, $a[0], 'Initial value of element at 0 is 1');
+    $passByValue($a);
+    $this->assertEquals(1, $a[0], 'After passing by value, the value of element at 0 is still 1');
+  }
+
+  public function testArrayPassingByReference() {
+
+    $a = [1, 2];
+
+    $passByReference= function(&$array) {$array[0] = 100;};
+
+    $this->assertEquals(1, $a[0], 'Initial value of element at 0 is 1');
+    $passByReference($a);
+    $this->assertEquals(100, $a[0], 'After apssing by reference, the value of element at 0 is  100');
+  }
+
+  public function testArrayPassingByValueContainingObjects() {
+
+    $a = [new A(), new A()];
+
+    $passByValue = function($array) {$array[0]->foo = 100;};
+
+    $this->assertEquals(1, $a[0]->foo, 'Initial value of element at 0 is 1');
+    $passByValue($a);
+    $this->assertEquals(100, $a[0]->foo, 'Passing by value does still changes the value if value is object\'s property.');
+  }
+
+  public function testArrayObjectPassingByValue() {
+
+    $a = new ArrayObject([1, 2]);
+
+    $passByValue = function($array) {$array[0] = 100;};
+
+    $this->assertEquals(1, $a[0], 'Initial value of element at 0 is 1');
+    $passByValue($a);
+    $this->assertEquals(100, $a[0], 'Passing by array object by value makes possible to mutate original\'s values.');
+  }
+
+  public function testObjectPassingByValue() {
+    $a = new A();
+
+    $passByValue = function($object) {$object->foo = 100;};
+
+    $this->assertEquals(1, $a->foo, 'Initial value of foo is 1');
+    $passByValue($a);
+    $this->assertEquals(100, $a->foo, 'Passing object by value and mutating it\'s property changes the original object.');
+
+  }
+
+  public function testObjectPassingByReference() {
+    $a = new A();
+
+    $passByValue = function($object) {$object->foo = 100;};
+
+    $this->assertEquals(1, $a->foo, 'Initial value of foo is 1');
+    $passByValue($a);
+    $this->assertEquals(100, $a->foo, 'Passing object by reference and mutating it\'s property changes the original object.');
+
+  }
+}
